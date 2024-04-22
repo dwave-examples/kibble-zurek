6763ba082dce41ccd96acce29a81b3a942e2a63c
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2024-04-22 09:48:22 -0700

    Unit test callback ``display_graphics_kink_density`` part 2

--- tests/test_cb_graph_kink_density.py
+++ tests/test_cb_graph_kink_density.py
@@ -25,14 +25,14 @@ import dimod
 
 from app import display_graphics_kink_density
 
+kz_graph_display = ContextVar('kz_graph_display')
 job_submit_state = ContextVar('job_submit_state')
-embeddings_cached = ContextVar('embeddings_cached')
 
 json_embeddings_file = { \
     "512": {"1": [11], "0": [10], "2": [12]}, \
     "5": {"1": [11], "0": [10], "2": [12], "3": [13], "4": [14]} }
 
-kz_graph_display = plotly.graph_objects.Figure({
+sample_vs_theory = plotly.graph_objects.Figure({
     'data': [{
               'type': 'scatter',
               'x': np.array([1, 2, 3], dtype=np.int64),
@@ -50,31 +50,44 @@ samples = dimod.as_samples([
     [-1, -1, -1, +1, +1],])
 sampleset = dimod.SampleSet.from_samples(samples, 'SPIN', 0)
 
-parametrize_vals = [('SUBMITTED'), ('COMPLETED')]
+parametrize_vals = [('kz_graph_display.value', 'both', 'dummy'), 
+                    ('kz_graph_display.value', 'kink_density', 'dummy'),
+                    ('kz_graph_display.value', 'schedule', 'dummy'),
+                    ('coupling_strength.value', 'schedule', 'dummy'),
+                    ('quench_schedule_filename.children', 'schedule', 'dummy'),
+                    ('job_submit_state.children', 'dummy', 'SUBMITTED'),
+                    ('job_submit_state.children', 'dummy', 'PENDING'),
+                    ('job_submit_state.children', 'dummy', 'COMPLETED')]
 
-@pytest.mark.parametrize('job_submit_state_val', parametrize_vals)
-def test_graph_kink_density_job_trigger(mocker, job_submit_state_val):
-    """Test graph of kink density: job-state trigger."""
+@pytest.mark.parametrize('trigger_val, kz_graph_display_val, job_submit_state_val', 
+                         parametrize_vals)
+def test_graph_kink_density(mocker, trigger_val, kz_graph_display_val, job_submit_state_val):
+    """Test graph of kink density."""
 
     mocker.patch('app.get_samples', return_value=sampleset)
 
     def run_callback():
         context_value.set(AttributeDict(**
-            {'triggered_inputs': [{'prop_id': 'job_submit_state.children'},]}))
+            {'triggered_inputs': [{'prop_id': trigger_val},]}))
 
-        return display_graphics_kink_density("both", 2.5, "schedule_filename", 
-                                          job_submit_state.get(), '1234', 5, 100, 7, 5, 
-                                          embeddings_cached.get(), kz_graph_display)
+        return display_graphics_kink_density(
+            kz_graph_display.get(), 2.5, "FALLBACK_SCHEDULE.csv", job_submit_state.get(), '1234', 
+            5, 100, 7, 5, json_embeddings_file, sample_vs_theory)
 
 
+    kz_graph_display.set(kz_graph_display_val)
     job_submit_state.set(job_submit_state_val)
-    embeddings_cached.set(json_embeddings_file)
 
     ctx = copy_context()
 
     output = ctx.run(run_callback)
 
-    if job_submit_state_val == 'COMPLETED':
+    if trigger_val in [
+        'kz_graph_display.value', 
+        'coupling_strength.value', 
+        'quench_schedule_filename.children']:
+        assert type(output) == plotly.graph_objects.Figure
+    elif job_submit_state_val == "COMPLETED":
         assert type(output) == plotly.graph_objects.Figure
     else:
-        output = no_update
+        assert output == no_update
