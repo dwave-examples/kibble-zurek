897c3c519f71504e7310ef0d8d167f59dbb62240
Andy Zhang <azhang@dwavesys.com>
2024-11-26 11:28:31 -0800

    Changed quench time input box to drop down menue

--- helpers/layouts_components.py
+++ helpers/layouts_components.py
@@ -22,13 +22,18 @@ __all__ = ['config_anneal_duration', 'config_kz_graph', 'config_spins',
 
 ring_lengths = [512, 1024, 2048]
 
-config_anneal_duration = Input(
+config_anneal_duration = dcc.Dropdown(
     id='anneal_duration',
-    type='number',
-    min=5,
-    max=100,
-    step=1,
-    value=7,
+    options=[
+        {'label': '5 µs', 'value': 5},
+        {'label': '10 µs', 'value': 10},
+        {'label': '20 µs', 'value': 20},
+        {'label': '40 µs', 'value': 40},
+        {'label': '80 µs', 'value': 80},
+        {'label': '160 µs', 'value': 160},
+        {'label': '320 µs', 'value': 320},
+    ],
+    value=5, # default value
     style={'max-width': '95%'}
 )
 
