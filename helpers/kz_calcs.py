9612ec140b4053e39a19eb1d0a80a36ddb016d4e
Andy Zhang <azhang@dwavesys.com>
2024-12-06 12:02:17 -0800

    Minor reformat

--- helpers/kz_calcs.py
+++ helpers/kz_calcs.py
@@ -14,54 +14,57 @@
 
 import numpy as np
 
-__all__ = ['kink_stats', 'theoretical_kink_density']
+__all__ = ["kink_stats", "theoretical_kink_density"]
+
 
 def theoretical_kink_density(annealing_times_ns, J, schedule, schedule_name):
     """
-    Calculate the kink density predicted for given the coupling strength and annealing times. 
+    Calculate the kink density predicted for given the coupling strength and annealing times.
 
     Args:
         annealing_times_ns: Iterable of annealing times, in nanoseconds.
 
         J: Coupling strength between the spins of the ring.
 
-        schedule: Anneal schedule for the selected QPU. 
+        schedule: Anneal schedule for the selected QPU.
 
-        schedule_name: Filename of anneal schedule. Used to compensate for schedule energy 
-        overestimate. 
+        schedule_name: Filename of anneal schedule. Used to compensate for schedule energy
+        overestimate.
 
     Returns:
-        Kink density per anneal time, as a NumPy array.  
+        Kink density per anneal time, as a NumPy array.
     """
 
     # See the Code section of the README.md file for an explanation of the
-    # following code. 
+    # following code.
 
-    COMPENSATION_SCHEDULE_ENERGY = 0.8 if 'Advantage_system' in schedule_name else 1.0
+    COMPENSATION_SCHEDULE_ENERGY = 0.8 if "Advantage_system" in schedule_name else 1.0
 
-    A = COMPENSATION_SCHEDULE_ENERGY * schedule['A(s) (GHz)']
-    B = COMPENSATION_SCHEDULE_ENERGY * schedule['B(s) (GHz)']         
-    s = schedule['s']
+    A = COMPENSATION_SCHEDULE_ENERGY * schedule["A(s) (GHz)"]
+    B = COMPENSATION_SCHEDULE_ENERGY * schedule["B(s) (GHz)"]
+    s = schedule["s"]
 
-    A_tag = A.diff()/s.diff()       # Derivatives of the energies for fast anneal 
-    B_tag = B.diff()/s.diff()
+    A_tag = A.diff() / s.diff()  # Derivatives of the energies for fast anneal
+    B_tag = B.diff() / s.diff()
 
-    sc_indx = abs(A - B*abs(J)).idxmin()    # Anneal fraction, s, at the critical point
+    sc_indx = abs(A - B * abs(J)).idxmin()  # Anneal fraction, s, at the critical point
 
-    
-    b_numerator  = 1e9 * np.pi * A[sc_indx] # D-Wave's schedules are in GHz
-    b_denominator  = B_tag[sc_indx]/B[sc_indx] - A_tag[sc_indx]/A[sc_indx]
-    b = b_numerator / b_denominator 
+    b_numerator = 1e9 * np.pi * A[sc_indx]  # D-Wave's schedules are in GHz
+    b_denominator = B_tag[sc_indx] / B[sc_indx] - A_tag[sc_indx] / A[sc_indx]
+    b = b_numerator / b_denominator
 
-    return np.power([1e-9 * t for t in annealing_times_ns], -0.5) / (2 * np.pi * np.sqrt(2 * b))
+    return np.power([1e-9 * t for t in annealing_times_ns], -0.5) / (
+        2 * np.pi * np.sqrt(2 * b)
+    )
+
 
 def kink_stats(sampleset, J):
     """
-    Calculate kink density for the sample set. 
+    Calculate kink density for the sample set.
 
     Calculation is the number of sign switches per sample divided by the length
     of the ring for ferromagnetic coupling. For anti-ferromagnetic coupling,
-    kinks are any pairs of identically-oriented spins. 
+    kinks are any pairs of identically-oriented spins.
 
     Args:
         sampleset: dimod sample set.
@@ -69,22 +72,22 @@ def kink_stats(sampleset, J):
         J: Coupling strength between the spins of the ring.
 
     Returns:
-        Switches/non-switches per sample and the average kink density across 
-        all samples.  
+        Switches/non-switches per sample and the average kink density across
+        all samples.
     """
     samples_array = sampleset.record.sample
-    sign_switches = np.diff(samples_array, 
-                            prepend=samples_array[:,-1].reshape(len(samples_array), 1))
-    
+    sign_switches = np.diff(
+        samples_array, prepend=samples_array[:, -1].reshape(len(samples_array), 1)
+    )
+
     if J < 0:
 
         switches_per_sample = np.count_nonzero(sign_switches, 1)
         kink_density = np.mean(switches_per_sample) / sampleset.record.sample.shape[1]
 
         return switches_per_sample, kink_density
-    
-    non_switches_per_sample = np.count_nonzero(sign_switches==0, 1)
+
+    non_switches_per_sample = np.count_nonzero(sign_switches == 0, 1)
     kink_density = np.mean(non_switches_per_sample) / sampleset.record.sample.shape[1]
 
     return non_switches_per_sample, kink_density
-    
--- helpers/tooltips.py
+++ helpers/tooltips.py
@@ -13,35 +13,25 @@
 #    limitations under the License.
 
 tool_tips = {
-    "anneal_duration":
-f"""Duration of the quantum anneal. Range of 5 to 320 nanoseconds.""",
-    "kz_graph_display":
-f"""Plot selection: Defects vs anneal duration or defects vs noise level""",
-    "spins":
-f"""Number of spins in the 1D ring.""",
-    "coupling_strength":
-f"""Coupling strength, J, between spins in the ferromagnetic ring. 
+    "anneal_duration": f"""Duration of the quantum anneal. Range of 5 to 320 nanoseconds.""",
+    "kz_graph_display": f"""Plot selection: Defects vs anneal duration or defects vs noise level""",
+    "spins": f"""Number of spins in the 1D ring.""",
+    "coupling_strength": f"""Coupling strength, J, between spins in the ferromagnetic ring. 
 Range of -1.8 to -0.6.
 """,
-    "qpu_selection":
-f"""Selection from quantum computers available to your account/project token.""",
-    "embedding_is_cached":
-f"""Whether or not a minor-embedding is cached for the selected QPU, for each 
+    "qpu_selection": f"""Selection from quantum computers available to your account/project token.""",
+    "embedding_is_cached": f"""Whether or not a minor-embedding is cached for the selected QPU, for each 
 of the available number of spins. If not available, an attempt is made to find
 an embedding the first time you submit a problem. 
 """,
-    "btn_simulate":
-f"""Click to (minor-embed if a cached embedding is unavailable) and 
+    "btn_simulate": f"""Click to (minor-embed if a cached embedding is unavailable) and 
 submit the problem to your selected QPU.
 """,
-    "quench_schedule_filename":
-f"""CSV file with the fast-anneal schedule for the selected quantum computer.
+    "quench_schedule_filename": f"""CSV file with the fast-anneal schedule for the selected quantum computer.
 If none exists, uses one from a different quantum computer (expect inaccuracies).
 You can download schedules from
 https://docs.dwavesys.com/docs/latest/doc_physical_properties.html
 """,
-    "job_submit_state":
-f"""Status of the last submission to the quantum computer (or initial state).""",
-    "btn_reset":
-f"""Clear all existing data stored for the current run and reset all plots.""",
+    "job_submit_state": f"""Status of the last submission to the quantum computer (or initial state).""",
+    "btn_reset": f"""Clear all existing data stored for the current run and reset all plots.""",
 }
